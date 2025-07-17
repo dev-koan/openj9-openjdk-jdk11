b6764345e0e482a705140b8feac576e2fcca9cfa
Dev Agarwal <devagarwal0508@gmail.com>
2025-07-17 16:14:38 -0400

    openjceplus first on the list

--- closed/src/jdk.crypto.ec/share/classes/sun/security/ec/NativeXDHKeyAgreement.java
+++ closed/src/jdk.crypto.ec/share/classes/sun/security/ec/NativeXDHKeyAgreement.java
@@ -158,8 +158,17 @@ public class NativeXDHKeyAgreement extends KeyAgreementSpi {
                     "Only two party agreement supported, lastPhase must be true");
             }
 
-            if (!(key instanceof XDHPublicKeyImpl)) {
+            if (!(key instanceof XECPublicKey)) {
                 throw new InvalidKeyException("Unsupported key type");
+            }
+
+            if (!(key instanceof XDHPublicKeyImpl)) {
+                try {
+                    XDHKeyFactory kf = new XDHKeyFactory();
+                    key = kf.engineTranslateKey(key);
+                } catch (Exception exception) {
+                    throw new InvalidKeyException("KeyFactory is not working as expected", exception);
+                }
             }
 
             XDHPublicKeyImpl publicKey = (XDHPublicKeyImpl) key;
